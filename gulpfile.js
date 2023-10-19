e2d63f8680f56a683c82235d7a3a0e6dc33cdba7
Vladimir Kantarevic <kantarevicvladimir@gmail.com>
2023-10-19 08:37:11 +0200

    responsive design finished

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+gulp.task("watch", function () {
+    // Define the files to watch
+    gulp.watch("./css/*/.scss", gulp.series("sass"));
+});
+
+gulp.task("sass", function () {
+    return gulp
+        .src("/css/*/.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest("./test"));
+});
+
+gulp.task("default", gulp.series("watch"));
